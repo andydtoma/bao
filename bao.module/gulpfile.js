478aa77d423f881e9d7d166f8c9de0c8af3e084e
Andy Daniel TOMA <andydtoma@hotmail.com>
2016-05-11 11:38:11 +0200

    Add bao.module Powershell Module Project

---
+++ bao.module/gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var del = require('del');
+var filelog = require('gulp-filelog');
+var fs = require('fs');
+var path = require('path');
+var insert = require('gulp-insert');
+var merge = require('merge-stream');
+
